3c361ed487cbd1adac12ac8907dcc5d4241f7514
arriolaleo <riblarsv@gmail.com>
2021-06-28 11:52:50 +0600

    imagen main is working still problems with router react dom

--- main/frontend/src/components/layouts/menu.js
+++ main/frontend/src/components/layouts/menu.js
@@ -3,43 +3,20 @@ import React from 'react'
 function Menu(){
     return(
         <>
-            <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href="./" class="navbar-brand">Bootstrap Menu</a>
-    </div>
-    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Mission</a></li>
-            <li><a href="#">Vision</a></li>
-            <li><a href="#">Careers</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Products</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li class="active">
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-</header>
+          <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="#"><img src="assets/img/icons/logo.png" alt="" width="30"/><span class="text-1000 fs-1 ms-2 fw-medium">Mario<span class="fw-bold">Arriola</span></span></a><button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
+              <li class="nav-item"><a class="nav-link active active" aria-current="page" href="#">About</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Pricing </a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Testimonials</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Help </a></li>
+            </ul>
+            <form class="d-flex py-3 py-lg-0"><button class="btn btn-link text-1000 fw-medium order-1 order-lg-0" type="button">Sign in</button><button class="btn btn-outline-danger rounded-pill order-0" type="submit">Sign Up</button></form>
+          </div>
+        </div>
+      </nav>
         </>
     )
 }
