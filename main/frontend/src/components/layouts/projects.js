a1c9a130889142e59b6099a92a940caf5bc39ab5
arriolaleo <riblarsv@gmail.com>
2021-06-29 12:34:11 +0600

    are ready but it is needed to add the tests

--- main/frontend/src/components/layouts/projects.js
+++ main/frontend/src/components/layouts/projects.js
@@ -1,26 +1,70 @@
-import React , { Component } from 'react';
+import React,{ useState, useEffect} from 'react';
 
-export class Projects extends Component{
-  render(){
+function Projects() {
+    const [projects , setProjects] = useState([])
+    const url = "/api/projects/"
+    useEffect(()=>{
+        async function getData(){
+            const response = await fetch(url)
+            const data = await response.json()
+            setProjects(data)
+        }
+        getData()
+    },[])
+    let  printProjects = projects.map( project => {
+        return(
+ <div class="col-1-of-3">
+                    <div class="card">
+                        <div class="card__side card__side--front">
+                            <div class="card__picture card__picture--2">
+                                 
+                            </div>
+                            <h4 class="card__heading">
+                                <span class="card__heading-span card__heading-span--2">The Sea Explore</span>
+
+                            </h4>
+                            <div class="card__details">
+                                <ul>
+                                    <li>3 day tours</li>
+                                    <li>Up to 30 people</li>
+                                    <li>2 tour guides</li>
+                                    <li>Sleep in cozy hotels</li>
+                                    <li>Difficulty: easy</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card__side card__side--back card__side--back-2">
+                            <div class="card__cta">
+                                <div class="card__price-box">
+                                    <p class="card__price-only">Only</p>
+                                    <p class="card__price-value">$295</p>
+                                </div>
+                                <a href="#0" class="btn btn--white">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+        )
+    })
     return(
 			<>
-        <div class="card mb-3">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." alt="..."/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-			</>
+			<section class="section-tours">
+            <div class=" text-center  u-margin-bottom-medium">
+                <br/>  <br/>  <h2 class="heading-secondary">
+                    Most Popular Tours
+                </h2>
+                <br/>  <br/>
+            </div>
+            <div class="row justify-content-center">
+                {printProjects}
+            </div>
+            <div class="text-center u-margin-top-big">
+                <a href="#0" class="btn btn--blue">Discover All Tour</a>
+            </div>
+        </section>
+      </>
     )
-  }
 }
 
 export default Projects
